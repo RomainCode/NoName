3892eb8030018c54b8f086d901c58e0dfd24c053
RomainCode <romain.cortale@gmail.com>
2022-06-30 15:31:53 +0200

    structure

--- game/game.py
+++ game/game.py
@@ -1,5 +1,11 @@
+from scene import Scene
+
 class Game:
 
-    def __init__(self) -> None:
+    def __init__(self):
         self.isNeedToClose = False
         pass
+
+
+        self.game_start_scene = Scene()
+        self.main_game_scene = Scene()
